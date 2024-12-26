d91863474ec59de1630202c8a9ec989cb3f56168
moveon0 <128313692+moveon0@users.noreply.github.com>
2024-12-27 00:02:34 +0900

    Update 프로세스.py

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,11 +1,11 @@
 def solution(priorities, location):
-    queue =  [(i,p) for i,p in enumerate(priorities)]
+    queue =  [(i,p) for i,p in enumerate(priorities)] #enumerate로 인덱스와 값 저장
     answer = 0
     while True:
-        cur = queue.pop(0)
-        if any(cur[1] < q[1] for q in queue):
+        cur = queue.pop(0)                      #맨 앞 요소 
+        if any(cur[1] < q[1] for q in queue):   #추출한 요소가 다음 요소보다 작으면 다시 넣음
             queue.append(cur)
         else:
-            answer += 1
-            if cur[0] == location:
+            answer += 1                         #인덱스 증가
+            if cur[0] == location:              #추출값이 목표값과 같으면 반환
                 return answer
